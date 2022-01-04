e809dbf6f7c72b37598b63f34c2a42dcd34d1a0f
rotarualexxxx95 <rotarualexxxx95@gmail.com>
2022-01-04 16:35:11 +0200

    Fix error middleware

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -15,7 +15,7 @@ const errorConverter = (err, req, res, next) => {
   next(error);
 };
 
-const errorHandler = (err, req, res) => {
+const errorHandler = (err, req, res, next) => {
   let { statusCode, message } = err;
   if (config.env === 'production' && !err.isOperational) {
     statusCode = httpStatus.INTERNAL_SERVER_ERROR;
